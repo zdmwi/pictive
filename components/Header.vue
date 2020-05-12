35cbe88c4b1a639e91ffe40091a30f00c0619dc1
Zidane <zidane.wright@speurgroup.com>
2020-05-11 22:55:12 -0500

    design basic profile layout

--- components/Header.vue
+++ components/Header.vue
@@ -1,18 +1,34 @@
 <template>
-  <div class="flex justify-between">
-    <div class="icon">R</div>
-    <div class="flex justify-between">
-      <div class="icon"></div>
+  <div class="flex shadow-md justify-between items-center p-4 bg-white">
+    <div class="flex items-center">
+      <h1 class="text-lg font-bold text-gray-800 mr-4">relatable.</h1>
+      <form @submit.prevent="handleSubmitQuery">
+        <input
+          v-model="searchString"
+          placeholder="Search relatable..."
+          type="text"
+          class="form-input focus:outline-none bg-gray-300 text-gray-800 border-0 w-64 rounded-lg text-sm"
+        />
+      </form>
     </div>
+    <div class="flex bg-gray-300 h-8 w-8 rounded-full cursor-pointer"></div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      searchString: ''
+    }
+  },
+  methods: {
+    handleSubmitQuery() {
+      console.log(this.searchString)
+    }
+  }
+}
 </script>
 
 <style>
-.icon {
-  @apply h-16 w-16 bg-blue-400;
-}
 </style>
---
+++ pages/user/_id.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container flex justify-between px-8 py-4">
+    <div class="mr-8">
+      <ProfileCard />
+      <FriendCollection class="mt-4" />
+    </div>
+    <div class="flex flex-col flex-1">
+      <PostForm />
+      <div
+        v-for="num in [1,2,3,4,5,6,7,8,9]"
+        class="w-full h-32 bg-white shadow-sm rounded-lg mt-4"
+      ></div>
+    </div>
+    <div class="ml-8">
+      <Gallery />
+    </div>
+  </div>
+</template>
+
+<script>
+import ProfileCard from '@/components/profile/ProfileCard.vue'
+import FriendCollection from '@/components/profile/FriendCollection.vue'
+import PostForm from '@/components/shared/PostForm.vue'
+import Gallery from '@/components/shared/Gallery.vue'
+
+export default {
+  layout: 'profile',
+  validate({ params }) {
+    return /^\d+/.test(params.id)
+  },
+  components: {
+    ProfileCard,
+    PostForm,
+    Gallery,
+    FriendCollection
+  }
+}
+</script>
+
+<style>
+</style>
