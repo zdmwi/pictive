b66123de1194e440d51ed633a7e495bf8f2c53ea
Zidane <zidane.wright@speurgroup.com>
2020-05-12 19:05:37 -0500

    change login credentials

--- pages/index.vue
+++ pages/index.vue
@@ -3,12 +3,12 @@
     <h3 class="block text-gray-700 font-bold mb-2">LOGIN</h3>
     <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
       <div class="mb-4">
-        <label class="block text-gray-700 text-sm font-bold mb-2" for="username">Username</label>
+        <label class="block text-gray-700 text-sm font-bold mb-2" for="email">Email Address</label>
         <input
           class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          id="username"
+          id="email"
           type="text"
-          placeholder="Username"
+          placeholder="someone@example.com"
         />
       </div>
       <div class="mb-6">
@@ -17,7 +17,7 @@
           class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
           id="password"
           type="password"
-          placeholder="****"
+          placeholder="********"
         />
       </div>
       <div class="flex items-center justify-around">
